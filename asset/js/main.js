3406a7531a336b1ab44a491a0f8f47e854fc5fc0
tiaz0128 <tiaz0128.dev@gmail.com>
2021-08-15 23:09:32 +0900

    Refactor opening_section

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,53 +1,60 @@
-function fadeTitelBackground(){
+import { OPENING } from "./literal.js"
+
+function changeFadeTarget(fadeImgs){
+  fadeImgs.some((img, idx) => {
+    if (img.classList.contains('active')) {
+      const target = idx === fadeImgs.length - 1 ? 0 : idx + 1
+
+      img.classList.remove('active');
+      fadeImgs[target].classList.add('active')
+      return true
+    }
+  })
+}
+
+function fadeOpeningBackground(){
   setInterval(() => {
     const fadeImgs = Array.from(document.querySelectorAll('.fade-img'))
-    
-    fadeImgs.some((img, idx) => {
-      if (img.classList.contains('active')) {
-        img.classList.remove('active');
-        idx === fadeImgs.length - 1 ? fadeImgs[0].classList.add('active') : fadeImgs[idx + 1].classList.add('active')
-        return true
-      }
-    })
+    changeFadeTarget(fadeImgs)
   }, 3500);
 }
 
-// 오프닝 타이틀
-function typpingText(){
-  const text = "체계적인 수업과 전문 지도자가 함께합니다.\n프로기사 40명 이상 배출";
+function tpyingOpeingDescription(chars){
+  const description = document.querySelector(".title-container .description")
+  const ch = chars.shift();
+  description.innerHTML += ch.charCodeAt() === 10 ? '<br/>' : ch
+}
+
+function typingOpeingTitle(){
+  const boldDesc = document.querySelector(".title-container .blod-description");
+  boldDesc.innerText = OPENING.STRONG;
+  boldDesc.classList.add('show');
+
+  const title = document.querySelector(".title-container .title");
+  title.innerText = OPENING.TITLE;
+  title.classList.add('toBig');
+}
+
+function playOpeingText(){
+  const text = OPENING.DESCRIPTION;
   const chars = text.split("");
-  let typedText = ''
 
   const intervalID = setInterval(() => {
     if (chars.length > 0) {
-      typedText += chars.shift();
-      document.querySelector(".title-container .description").innerText = typedText;
+      tpyingOpeingDescription(chars)
     } else {
       clearInterval(intervalID);
-
-      const boldDesc = document.querySelector(".title-container .blod-description");
-      boldDesc.innerText = "20년 전통의";
-      boldDesc.classList.add('show');
-
-      // 학원명 등장.
-      const title = document.querySelector(".title-container .title");
-      title.innerText = "양천대일 바둑학원";
-      title.classList.add('toBig');
-
+      
+      typingOpeingTitle()
       playArrow()
     }
   }, 80);
 }
 
-
 function playArrow(){
-  const intervalID = setInterval(() => {
-    clearInterval(intervalID);
-
-    setTimeout(() => {
-      document.querySelector(".title-arrow").style.display = "block";
-    }, 1000);
-  }, 60);
+  setTimeout(() => {
+    document.querySelector(".title-arrow").style.display = "block";
+  }, 1000);
 }
 
-export {typpingText, fadeTitelBackground, playArrow }
+export { playOpeingText, fadeOpeningBackground, playArrow }
